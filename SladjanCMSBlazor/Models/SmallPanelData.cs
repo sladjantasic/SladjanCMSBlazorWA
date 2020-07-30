fb29d7e728547ac23bae6be7f034199e961858a6
sladjantasic <sladjan.m.tasic@gmail.com>
2020-07-30 17:12:40 +0200

    panel

---
+++ SladjanCMSBlazor/Models/SmallPanelData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SladjanCMSBlazor.Models
+{
+    public class SmallPanelData
+    {
+        public string Title { get; set; }
+        public string CurrentValue { get; set; }
+        public string MaxValue { get; set; }
+    }
+}
--- SladjanCMSBlazor/wwwroot/js/app.js
+++ SladjanCMSBlazor/wwwroot/js/app.js
@@ -1,1 +1,24 @@
 ﻿
+populateChart = () => {
+    $(new Morris.Line({
+        // ID of the element in which to draw the chart.
+        element: 'linechart',
+        // Chart data records -- each entry in this array corresponds to a point on
+        // the chart.
+        data: [
+            { year: '2008', value1: 20, value2: 34 },
+            { year: '2009', value1: 10, value2: 44 },
+            { year: '2010', value1: 5, value2: 44 },
+            { year: '2011', value1: 5, value2: 46 },
+            { year: '2012', value1: 20, value2: 65 }
+        ],
+        // The name of the data record attribute that contains x-values.
+        xkey: 'year',
+        // A list of names of data record attributes that contain y-values.
+        ykeys: ['value1', 'value2'],
+        // Labels for the ykeys -- will be displayed when you hover over the
+        // chart.
+        labels: ['Value1', 'Value2'],
+        lineColors: ['rgb(139, 27, 248)', 'rgb(241, 73, 27)']
+    }));
+}
